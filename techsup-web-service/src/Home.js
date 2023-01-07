0852e39d0d7e7f9ff5b26d57158c91b6e440fdcf
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-01-08 00:07:54 +0300

    Home Page last edit

--- techsup-web-service/src/Home.js
+++ techsup-web-service/src/Home.js
@@ -14,14 +14,20 @@ function Home(){
                 <img className="home__image" src={require('./img/MainPage_Pt5.png')} /> */}
             </div><div className="home__row" >
                 {/* Product */}
+                <Product title='ARC 770/Intel Core i5 12400F/INTEL Arc A770 16GB GDDR6/16GB DDR4/512GB M.2 NVMe SSD/INTEL ARC PC'
+                price='19.000' image='./img/Product_Img1.png' rating={4}/>
+                {/* Product */}
+                <Product />
+            </div><div className="home__row" >
+                {/* Product */}
                 <Product />
                 {/* Product */}
+                <Product />
+                {/* Product */}
+                <Product />
             </div><div className="home__row" >
                 {/* Product */}
-                {/* Product */}
-                {/* Product */}
-            </div><div className="home__row" >
-                {/* Product */}
+                <Product /> 
             </div>
         </div>
     )
--- techsup-web-service/src/Product.js
+++ techsup-web-service/src/Product.js
@@ -2,22 +2,25 @@
 import React from "react";
 import './Product.css';
 
-function Product (){
+function Product ({title,image,price,rating}){
     return (
     <div className="product" >
         <div className="product__info" >
-            <p> ARC 770/Intel Core i5 12400F/INTEL Arc A770 16GB GDDR6/16GB DDR4/512GB M.2 NVMe SSD/INTEL ARC PC </p>
-            <p> Product Price : 
-                <strong>19.000</strong>
+            <p> {title} </p>
+            <p className= "product__price"> Product Price : 
+                <strong>{price}</strong>
                 <small>₺</small>
             </p>
             <div className="product__rating">
-                <p>⭐</p>
-                <p>⭐</p>
-                <p>⭐</p>
+                {Array(rating)
+                .fill()
+                .map((_,i) => {
+                    return <p>⭐</p>
+                })}
             </div>
         </div>
         <img className='' src={require('./img/Product_Img1.png')} />
+        <button className="button"> Add to Basket</button>
     </div>
     )
 }
