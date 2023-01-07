7261e1dc48c61dd13756b49ba6f99c738242e084
uygurberkay <50241715+uygurberkay@users.noreply.github.com>
2023-01-07 16:18:03 +0300

    Last update made

--- techsup-web-service/src/App.js
+++ techsup-web-service/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header';
+
+// import Header from './Header';
 
 function App() {
   return (
+    // BEM  (Come Alive)
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* Header render imported above */}
+    <Header /> 
+    {/* Home */}
     </div>
   );
 }
